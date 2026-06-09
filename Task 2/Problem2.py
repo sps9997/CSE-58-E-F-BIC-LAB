d844fe1a2845ac06f5f4998c9454bad85f740f32
ShongeetSaha189 <140524543+sps9997@users.noreply.github.com>
2026-06-10 02:22:26 +0600

    Implement substring frequency count in DNA
    Count occurrences of substrings of given length in DNA.

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,14 @@
 
+DNA=input().strip()
+S_LEN=int(input())
+counts={}
+
+
+for i in range (len(DNA) - S_LEN + 1):
+    subString = DNA[i:i + S_LEN]
+    counts[subString] = counts.get(subString,0) + 1
+
+
+max_count = max(counts.values())
+result = [subString for subString, count in counts.items() if count == max_count]
+print(*result)
