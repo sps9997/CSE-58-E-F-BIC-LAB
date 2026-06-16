0707d4564cd51a9ba6ad29106c64ef3ee2b813e1
ShongeetSaha189 <140524543+sps9997@users.noreply.github.com>
2026-06-16 16:19:58 +0600

    Implement k-mer matching with allowed mismatches
    Add functionality to find and print starting indices of k-mers in a sequence based on a maximum allowed mismatches.

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,17 @@
+k_mers= input()
+seq= input()
+length= int(input())
 
+
+s_kmers= len(k_mers)
+s_seq= len(seq)
+
+for i in range (s_seq - s_kmers +1):
+    flag = 0
+
+    for j in range (s_kmers):
+        if (k_mers[j]!= seq[i+j]):
+            flag += 1
+
+    if (flag <= length):
+        print(i)
